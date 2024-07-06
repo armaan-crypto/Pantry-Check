a0870cb5581b8be03accb6284be56491356e2741
armaan-crypto <armaanahmed08@gmail.com>
2024-07-06 19:53:58 -0400

    Initial Commit

---
+++ Pantry Check/Pantry_CheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pantry_CheckApp.swift
+//  Pantry Check
+//
+//  Created by Armaan Ahmed on 7/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Pantry_CheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
